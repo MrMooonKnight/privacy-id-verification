477dea270e596b0a1104d003657ae87b10388cd0
Mr Talha <127784066+MrMooonKnight@users.noreply.github.com>
2025-04-18 17:11:46 +0500

    not much...

--- backend/backend/__init__.py
+++
@@ -1,9 +0,0 @@
-from ai_models.face_recognition_model import FaceRecognitionModel
-from ai_models.fraud_detection_model import FraudDetectionModel
-from ai_models.document_verification_model import DocumentVerificationModel
-
-__all__ = [
-    'FaceRecognitionModel',
-    'FraudDetectionModel',
-    'DocumentVerificationModel'
-] 
